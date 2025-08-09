faa6566731a93a28abb6d401b72b7094a59a4271
Daniel Mendonça <dani_sanca@hotmail.com>
2025-08-09 08:30:14 -0300

    Finalizado Versão 1

---
+++ MoneyExtension.Tests/MoneyExtensionsTests.cs
@@ -0,0 +1,15 @@
+﻿namespace MoneyExtension.Tests
+{
+    [TestClass]
+    public sealed class MoneyExtensionsTests
+    {
+        [TestMethod]
+        public void ShouldConvertDecilmalToInt()
+        {
+            decimal valor = 279.55m;
+            var cents = valor.ToCents();
+
+            Assert.AreEqual(27955,cents);
+        }
+    }
+}
